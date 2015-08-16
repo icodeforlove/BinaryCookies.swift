babb7b3ffc7d295b4b481fed4ae617898715dff1
Chad Scira <chadvscira@gmail.com>
2015-08-16 23:20:28 +0700

    Initial Commit

---
+++ Cookies/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Cookies
+//
+//  Created by Chad Scira on 8/16/15.
+//  Copyright © 2015 Chad Scira. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
